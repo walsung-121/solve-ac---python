329f0898e38e82ae1e8311c67922c6a72b88dc92
walsung-121 <nasangpa@gmail.com>
2026-01-09 01:20:06 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+a = int(input())
+if 90 <= a <= 100 and 100 >= a >= 0 :
+    print('A')
+elif 80 <= a <= 89 and 100 >= a >= 0 :
+    print('B')
+elif 70 <= a <= 79 and 100 >= a >= 0 :
+    print('C')
+elif 60 <= a <= 69 and 100 >= a >= 0 :
+    print('D')
+elif 59 >= a and 100 >= a >= 0 :
+    print('F')
